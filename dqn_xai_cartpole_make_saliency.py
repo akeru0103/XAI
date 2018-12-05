a57fd34bda6bca24632cc7ebf2a86f146a96ca13
akeru0103 <44463986+akeru0103@users.noreply.github.com>
2018-12-05 15:59:17 +0900

    saliency計算...cpu,それ以外...gpu(cpu)

--- dqn_xai_cartpole_make_saliency.py
+++ dqn_xai_cartpole_make_saliency.py
@@ -280,17 +280,17 @@ def save_image(image, save_name, save_type='png'):
     if os.path.exists('images')==False:
         os.mkdir('images')
     #imagesフォルダの作成
-    if image.dim()==3:
-        image = Image.fromarray(np.uint8(image.cpu().numpy().transpose(1, 2, 0)))
+    if image.ndim==3:
+        image = Image.fromarray(np.uint8(image.transpose(1, 2, 0)))
         #保存は[縦,横,色],0~255
         image.save('images/'+save_name+'.'+save_type)
-    elif image.dim()==2:
+    elif image.ndim==2:
         image_width = image.shape[1]
         image_hight = image.shape[0]
-        output_image = torch.zeros((image_hight, image_width, 3), dtype=torch.float)
+        output_image = np.zeros((image_hight, image_width, 3))
         for i in range(3):
             output_image[:,:, i] = image
-        image = Image.fromarray(np.uint8(output_image.numpy()))
+        image = Image.fromarray(np.uint8(output_image))
         #保存は[縦,横,色],0~255
         image.save('images/'+save_name+'.'+save_type)
 
@@ -362,17 +362,17 @@ def make_perturbed_image(image, perturbed_point, mask_sigma, blurred_sigma, save
 
     image_width = image.shape[2]
     image_hight = image.shape[1]
-    mask = torch.from_numpy(get_mask(perturbed_point, [image_hight,image_width], mask_sigma)).float().to(device)
+    mask = get_mask(perturbed_point, [image_hight,image_width], mask_sigma)
 
-    blurred_frame = torch.zeros((3, image_hight, image_width)).to(device)
-    image1 = torch.zeros((3, image_hight, image_width)).to(device)
-    image2 = torch.zeros((3, image_hight, image_width)).to(device)
-    image3 = torch.zeros((3, image_hight, image_width)).to(device)
+    blurred_frame = np.zeros((3, image_hight, image_width))
+    image1 = np.zeros((3, image_hight, image_width))
+    image2 = np.zeros((3, image_hight, image_width))
+    image3 = np.zeros((3, image_hight, image_width))
     #image3 = copy.copy(image)#np.zeros((3, image_hight, image_width))
 
     for i in range(3):
-        blurred_frame[i] = torch.from_numpy(gaussian_filter(image[i], sigma=blurred_sigma)).to(device)
-        image1[i] = image[i]*(1-mask)
+        blurred_frame[i] = gaussian_filter(image[i], sigma=blurred_sigma)
+        image1[i] = np.multiply(image[i],1-mask)
         image2[i] = blurred_frame[i]*mask
         image3[i] = image1[i] + image2[i]
 
@@ -388,7 +388,7 @@ def make_perturbed_image(image, perturbed_point, mask_sigma, blurred_sigma, save
     return image3
 
 def make_saliency_map(image, mask_sigma, blurred_sigma, decimation_rate):
-    #saliency_mapを作成(data:(色,縦,横), dtype:tensor, shape:(3, hight of image, width of image), range:0~0.1　)
+    #saliency_mapを作成(data:(色,縦,横), dtype:ndarray, shape:(3, hight of image, width of image), range:0~0.1　)
     #image:入力画像(data(?, 色, 縦, 横), dtype:tensor, shape:(1, 3, :, :), range:0~1)
     #mask_sigma:マスク画像のσ
     #blurred_sigma:ぼやかした画像のσ
@@ -399,12 +399,12 @@ def make_saliency_map(image, mask_sigma, blurred_sigma, decimation_rate):
     state_width = state.shape[3]
     state_hight = state.shape[2]
     normal_q = policy_net(image)
-    saliency_map = torch.zeros((int(state_hight/decimation_rate), int(state_width/decimation_rate) )).to(device)
+    saliency_map = np.zeros((int(state_hight/decimation_rate), int(state_width/decimation_rate) ))
     for i in range(0, state_width, decimation_rate):
         for j in range(0, state_hight, decimation_rate):
-            perturbed_state = make_perturbed_image( (image.squeeze())*255, [j,i], mask_sigma, blurred_sigma )
-            #perturbed_state = torch.from_numpy(perturbed_state)
-            perturbed_state = perturbed_state.unsqueeze(0).to(device)
+            perturbed_state = make_perturbed_image( (image.numpy().squeeze())*255, [j,i], mask_sigma, blurred_sigma )
+            perturbed_state = torch.from_numpy(perturbed_state)
+            perturbed_state = perturbed_state.unsqueeze(0).to(device).float()
             perturbed_q = policy_net(perturbed_state)
 
             #saliency_map[:, int(j/decimation_rate),int(i/decimation_rate)] = ( np.array(max_color)-np.array(min_color) )*( float((normal_q-perturbed_q).pow(2).sum().mul_(0.5)) /255 ) + np.array(min_color)*(1/255)*0.6
@@ -490,7 +490,7 @@ for i_episode in range(num_episodes):
         #print(episode_durations)
         #1エピソード目に行う処理
         if i_episode == 0 and t == 3:
-            make_perturbed_image(state.squeeze(),(20,40),4,3,save=True)
+            make_perturbed_image(state.squeeze().numpy(),(20,40),4,3,save=True)
             print(' filter images were saved')
             #フィルターのサンプル画像を保存
 
@@ -502,8 +502,8 @@ for i_episode in range(num_episodes):
         '''
 
         if saliency_save_flag == True: #i_episode=0,5,10...=1,6,11...エピソード目
-            saliency_map_sequence.append( make_saliency_map(state, 4, 3, saliency_calcuration_rate ).cpu().numpy() )
-            input_sequence.append( current_screen.cpu().numpy().squeeze() )
+            saliency_map_sequence.append( make_saliency_map(state, 4, 3, saliency_calcuration_rate ) )
+            input_sequence.append( current_screen.numpy().squeeze() )
 
         frame_num += 1
 
